28a6a4053c577e996112d7bc733a032d9538f303
xianyu190912414 <57263797+xianyu190912414@users.noreply.github.com>
2020-01-17 18:10:22 +0800

    Create Student.java

---
+++ 第五章/abstractClasses/Student.java
@@ -0,0 +1,17 @@
+public class Student extends Person {
+	private String major;
+	
+	/**
+	 * @param name the student's name
+	 * @param major the student's major
+	 */
+	public Student(String name, String major) {
+		//pass n to superclass constructor
+		super(name);
+		this.major = major;
+	}
+	
+	public String getDescription() {
+		return "a student major in " + major;
+	}
+}
