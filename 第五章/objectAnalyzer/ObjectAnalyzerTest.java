8be3fd87e6969f04f35762c1196ffe74e9b07947
xianyu190912414 <57263797+xianyu190912414@users.noreply.github.com>
2020-01-19 20:08:34 +0800

    Create ObjectAnalyzerTest.java

---
+++ 第五章/objectAnalyzer/ObjectAnalyzerTest.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+/**
+ * This program uses reflection to spy on objects
+ * @version 1.10 2020-1-19
+ * @author 19091
+ *
+ */
+public class ObjectAnalyzerTest {
+	public static void main(String[] args) {
+		ArrayList<Integer> squares = new ArrayList<>();
+		for(int i = 1; i <= 5; i ++) {
+			squares.add(i * i);
+		}
+		System.out.println(new ObjectAnalyzer().toString(squares));
+	}
+}
