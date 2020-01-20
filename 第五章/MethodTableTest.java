1c80d4ab3a72c59f4c82c9f0fa8f4a201d9e4ff6
xianyu190912414 <57263797+xianyu190912414@users.noreply.github.com>
2020-01-20 15:35:51 +0800

    Create MethodTableTest.java

---
+++ 第五章/MethodTableTest.java
@@ -0,0 +1,55 @@
+import java.lang.reflect.*;
+
+/**
+ * This program shows how to invoke methods through reflection.
+ * @version 1.10 2020-1-20
+ * @author 19091
+ *
+ */
+public class MethodTableTest {
+	public static void main(String[] args) {
+		//get method pointers to the square and sqrt methods.
+		try {
+			Method square = MethodTableTest.class.getMethod("square", double.class);
+			Method sqrt = Math.class.getMethod("sqrt", double.class);
+			
+			//print tables of x- and y-values.
+			printTable(1, 10, 10, square);
+			printTable(1, 10, 10, sqrt);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Returns the square of a number
+	 * @param x a number
+	 * @return x squared
+	 */
+	public static double square(double x) {
+		return x * x;
+	}
+	
+	/**
+	 * Prints a table with x- and y-values for a method
+	 * @param from the lower bound for the x_values
+	 * @param to the upper bound for the x_values
+	 * @param n the number of rows in the table
+	 * @param f a method with a double parameter and double return value
+	 */
+	public static void printTable(double from, double to, int n, Method f) {
+		//print out the method as table header
+		System.out.println(f);
+		double dx = (to - from) / (n - 1);
+		
+		for (double x = from; x <= to; x += dx) {
+			try {
+				double y = (Double) f.invoke(null, x);
+				System.out.printf("%10.4f | %10.4f%n", x, y);
+			}
+			catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
