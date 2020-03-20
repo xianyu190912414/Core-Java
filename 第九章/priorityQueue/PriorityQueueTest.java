1f1daeeff8ec4815dd4133bcab9bbdf0dda0e83a
xianyu190912414 <57263797+xianyu190912414@users.noreply.github.com>
2020-03-20 21:38:34 +0800

    Create PriorityQueueTest.java

---
+++ 第九章/priorityQueue/PriorityQueueTest.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.time.*;
+
+/**
+ * This program demonstrates the use of a priority queue.
+ * @version 1.10 2020-3-20
+ * @author 19091
+ *
+ */
+public class PriorityQueueTest {
+	public static void main(String[] args) {
+		PriorityQueue<LocalDate> pq = new PriorityQueue<>();
+		pq.add(LocalDate.of(1906, 12, 9));
+		pq.add(LocalDate.of(1815, 12, 10));
+		pq.add(LocalDate.of(1903, 12, 3));
+		pq.add(LocalDate.of(1910, 6, 22));
+		
+		System.out.println("Iterating over elements...");
+		for(LocalDate date : pq) 
+			System.out.println(date);
+		System.out.println("Removing elements...");
+		while(!pq.isEmpty())
+			System.out.println(pq.remove());
+	}
+}
