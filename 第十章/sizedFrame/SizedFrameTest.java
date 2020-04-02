585cf99be7e01a78603725c481f3e1067730396b
xianyu190912414 <57263797+xianyu190912414@users.noreply.github.com>
2020-04-02 15:55:32 +0800

    Create SizedFrameTest.java

---
+++ 第十章/sizedFrame/SizedFrameTest.java
@@ -0,0 +1,37 @@
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ * @version 1.10 2020-3-27
+ * @author 19091
+ *
+ */
+
+public class SizedFrameTest {
+	public static void main(String[] args) {
+		EventQueue.invokeLater(() -> {
+			JFrame frame = new SizedFrame();
+			frame.setTitle("SizedFrame");
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.setVisible(true);
+		});
+	}
+}
+
+class SizedFrame extends JFrame {
+	public SizedFrame() {
+		//get screen dimensions
+		Toolkit kit = Toolkit.getDefaultToolkit();
+		Dimension screenSize = kit.getScreenSize();
+		int screenHeight = screenSize.height;
+		int screenWidth = screenSize.width;
+		
+		//set frame height, width and let platform pick screen location
+		setSize(screenWidth / 2, screenHeight / 2);
+		setLocationByPlatform(true);
+		
+		//set frame icon
+		Image img = new ImageIcon("icon.gif").getImage();
+		setIconImage(img);
+	}
+}
