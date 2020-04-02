e0b710ec1866a58e34ef976056f2046fef8a0e7b
xianyu190912414 <57263797+xianyu190912414@users.noreply.github.com>
2020-04-02 15:57:28 +0800

    Create DrawTest.java

---
+++ 第十章/draw/DrawTest.java
@@ -0,0 +1,71 @@
+import java.util.*;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.geom.*;
+
+/**
+ * @version 1.10 2020-3-27
+ * @author 19091
+ *
+ */
+
+public class DrawTest {
+	public static void main(String[] args) {
+		EventQueue.invokeLater(() -> {
+			JFrame frame = new DrawFrame();
+			frame.setTitle("DrawTest");
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.setVisible(true);
+		});
+	}
+}
+
+/**
+ * A frame that contains a panel with drawing.
+*/
+class DrawFrame extends JFrame {
+	public DrawFrame() {
+		add(new DrawComponent());
+		setLocationByPlatform(true);
+		pack();
+	}
+}
+
+/**
+*A component that displays rectangles and ellipses.
+*/
+class DrawComponent extends JComponent {
+	private static final int DEFAULT_WIDTH = 400;
+	private static final int DEFAULT_HEIGHT = 400;
+	
+	public void paintComponent(Graphics g) {
+		Graphics2D g2 = (Graphics2D) g;
+		
+		//draw a rectangle
+		double leftX = 100;
+		double topY = 100;
+		double width = 200;
+		double height = 150;
+		
+		Rectangle2D rect = new Rectangle2D.Double(leftX, topY, width, height);
+		g2.draw(rect);
+		
+		//draw the enclosed ellipse
+		Ellipse2D ellipse = new Ellipse2D.Double();
+		ellipse.setFrame(rect);
+		g2.draw(ellipse);
+		
+		//draw a circle with the same center.
+		double centerX = rect.getCenterX();
+		double centerY = rect.getCenterY();
+		double radius = 150;
+		
+		Ellipse2D circle = new Ellipse2D.Double();
+		circle.setFrameFromCenter(centerX, centerY, centerX + radius, centerY + radius);
+		g2.draw(circle);
+	}
+	
+	public Dimension getPreferredSize() {
+		return new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+	}
+}
