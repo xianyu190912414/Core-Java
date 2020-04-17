f5377a7dd670bba126ddea24aa10ba46530496c0
xianyu190912414 <57263797+xianyu190912414@users.noreply.github.com>
2020-04-17 16:38:07 +0800

    Create SliderFrameTest.java

---
+++ 第十二章/slider/SliderFrameTest.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class SliderFrameTest {
+	public static void main(String[] args) {
+		SliderFrame frame = new SliderFrame();
+		frame.setTitle("SliderFrameTest");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+}
