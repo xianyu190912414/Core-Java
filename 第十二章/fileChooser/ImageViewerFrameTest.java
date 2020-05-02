f72e00e84335c685f82dadda2b195cd053c435bc
xianyu190912414 <57263797+xianyu190912414@users.noreply.github.com>
2020-05-02 10:50:03 +0800

    Create ImageViewerFrameTest.java

---
+++ 第十二章/fileChooser/ImageViewerFrameTest.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class ImageViewerFrameTest {
+	public static void main(String... args) {
+		ImageViewerFrame frame = new ImageViewerFrame();
+		frame.setTitle("ImageViewerFrameTest");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+}
