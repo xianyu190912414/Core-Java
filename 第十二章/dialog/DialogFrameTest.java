2713ab8f9a6f112d149a3eb770d77aea0f9efa35
xianyu190912414 <57263797+xianyu190912414@users.noreply.github.com>
2020-05-02 10:46:16 +0800

    Create DialogFrameTest.java

---
+++ 第十二章/dialog/DialogFrameTest.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+import 第十二章optionDialog.OptionDialogFrame;
+
+public class DialogFrameTest {
+	public static void main(String...  args) {
+		DialogFrame frame = new DialogFrame();
+		frame.setTitle("CircleLayoutFrame");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+}
