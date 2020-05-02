61f52cecc318d194d9577892f90509f42d31f66e
xianyu190912414 <57263797+xianyu190912414@users.noreply.github.com>
2020-05-02 10:56:19 +0800

    Create OptionDialogFrameTest.java

---
+++ 第十二章/optionDialog/OptionDialogFrameTest.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+import 第十二章circleLayout.CircleLayoutFrame;
+public class OptionDialogFrameTest {
+	public static void main(String... args) {
+		OptionDialogFrame frame = new OptionDialogFrame();
+		frame.setTitle("CircleLayoutFrame");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+}
