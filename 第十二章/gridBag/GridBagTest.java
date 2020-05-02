ef00a257456bf233f40eb0099d22477647927e39
xianyu190912414 <57263797+xianyu190912414@users.noreply.github.com>
2020-05-02 10:51:52 +0800

    Create GridBagTest.java

---
+++ 第十二章/gridBag/GridBagTest.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class GridBagTest {
+	public static void main(String[] args) {
+		FontFrame frame = new FontFrame();
+		frame.setTitle("GridBagTest");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+}
