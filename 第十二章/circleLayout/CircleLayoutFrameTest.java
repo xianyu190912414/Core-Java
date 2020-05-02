9b52b61e6e7c0b45a02101ac0b41b9c40a8971bb
xianyu190912414 <57263797+xianyu190912414@users.noreply.github.com>
2020-05-02 10:30:27 +0800

    Create CircleLayoutFrameTest.java

---
+++ 第十二章/circleLayout/CircleLayoutFrameTest.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class CircleLayoutFrameTest {
+	public static void main(String[] args) {
+		CircleLayoutFrame frame = new CircleLayoutFrame();
+		frame.setTitle("CircleLayoutFrame");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+}
