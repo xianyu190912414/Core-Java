fab94853672475f454a2143e2e1a78a189328dfd
xianyu190912414 <57263797+xianyu190912414@users.noreply.github.com>
2020-05-02 10:59:51 +0800

    Create ToolBarFrameTest.java

---
+++ 第十二章/toolBar/ToolBarFrameTest.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class ToolBarFrameTest {
+	public static void main(String[] args) {
+		ToolBarFrame frame = new ToolBarFrame();
+		frame.setTitle("ToolBarFrame");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+}
